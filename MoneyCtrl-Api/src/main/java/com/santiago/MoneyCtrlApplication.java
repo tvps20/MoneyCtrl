9bcb54d3e7630f3f436467c652f8bfff884101b1
Santiago <thiago.v.santiago@accenture.com>
2020-04-17 21:32:02 -0300

    started project

---
+++ MoneyCtrl-Api/src/main/java/com/santiago/MoneyCtrlApplication.java
@@ -0,0 +1,13 @@
+package com.santiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyCtrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyCtrlApplication.class, args);
+	}
+
+}
