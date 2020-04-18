0431f37d0f584c87c4501e28e9016f9a705aa653
Santiago <thiago.v.santiago@accenture.com>
2020-04-18 18:09:29 -0300

    add lombok in project

---
+++ MoneyCtrl-Api/src/main/java/com/santiago/domain/AbstractEntity.java
@@ -0,0 +1,82 @@
+package com.santiago.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//Não vai mapear como tabela no banco
+@MappedSuperclass
+@ToString
+public class AbstractEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter @Setter
+	protected Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@CreationTimestamp
+	@Getter @Setter
+    protected LocalDate createdAt;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@UpdateTimestamp
+	@Getter @Setter
+	protected LocalDate updatedAt;
+	
+	@Getter @Setter
+	protected boolean ativo = true;
+
+	
+	// Construtores
+	public AbstractEntity() {}
+	
+	public AbstractEntity(Long id) {
+		this.id = id;
+	}
+
+
+	// Metodos de Comparacao por valor
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractEntity other = (AbstractEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ MoneyCtrl-Api/src/main/java/com/santiago/endopoints/CartaoController.java
@@ -0,0 +1,22 @@
+package com.santiago.endopoints;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.santiago.domain.Cartao;
+
+@RestController
+@RequestMapping("/cartao")
+public class CartaoController {
+
+	@GetMapping
+	public ResponseEntity<List<Cartao>> listar(){
+		Cartao cartao = new Cartao(1L, "mastercard");
+		return ResponseEntity.ok().body(Arrays.asList(cartao));
+	}
+}
