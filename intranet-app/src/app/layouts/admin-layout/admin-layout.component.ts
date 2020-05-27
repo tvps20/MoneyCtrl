f92a31f03dab35719d3a88756a293b92f189572f
Santiago <thiago.v.santiago@accenture.com>
2020-05-27 09:48:25 -0300

    refactor code admin layout

--- intranet-app/src/app/layouts/admin-layout/admin-layout.component.ts
+++ intranet-app/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,29 +1,12 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { Location, LocationStrategy, PathLocationStrategy, PopStateEvent } from '@angular/common';
-import { Router, NavigationEnd, NavigationStart } from '@angular/router';
-import * as $ from "jquery";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-layout',
-  templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.css']
+    selector: 'app-admin-layout',
+    templateUrl: './admin-layout.component.html',
+    styleUrls: ['./admin-layout.component.css'],
 })
 export class AdminLayoutComponent implements OnInit {
+    constructor() {}
 
-  private lastPoppedUrl: string;
-  private yScrollStack: number[] = [];
-
-  constructor(public location: Location, private router: Router) { }
-
-  ngOnInit(): void {
-    
-  }
-
-  public isMaps(path) {
-    return true;
-  }
-
-  public isMac(): boolean {
-    return false;
-  }
+    ngOnInit(): void {}
 }
