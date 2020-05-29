10df4e3c3e9edc5a8f59d5f5c13001968d8468ab
Santiago <thiago.v.santiago@accenture.com>
2020-05-28 22:17:38 -0300

    remove imports

--- intranet-app/src/app/shared-import/shared-import.module.ts
+++ intranet-app/src/app/shared-import/shared-import.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatRippleModule, MatNativeDateModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -21,10 +18,6 @@ import { MatRadioModule } from '@angular/material/radio';
     FormsModule,
     FormsModule,
     ReactiveFormsModule,
-    MatButtonModule,
-    MatRippleModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatSelectModule,
     MatNativeDateModule,
     MatDatepickerModule,
@@ -37,10 +30,6 @@ import { MatRadioModule } from '@angular/material/radio';
     FormsModule,
     FormsModule,
     ReactiveFormsModule,
-    MatButtonModule,
-    MatRippleModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatSelectModule,
     MatNativeDateModule,
     MatDatepickerModule,
