dd81a15957bd30613f80dd90766684be1d48196a
Santiago <thiago.v.santiago@accenture.com>
2020-07-16 12:51:14 -0300

    add cors config

---
+++ api/src/main/java/com/santiago/moneyctrl/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.santiago.moneyctrl.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	 /**
+     * Metodo para liberar o acesso externo a requisições http
+     *
+     * @param registry
+     */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
+}
