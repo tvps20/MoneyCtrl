f0ce15d0945d4a9f5ffb1012327ec5c5b6ecdd2e
Santiago <thiago.v.santiago@accenture.com>
2020-07-24 10:52:01 -0300

    add user restriction

--- api/src/main/java/com/santiago/moneyctrl/services/UsuarioService.java
+++ api/src/main/java/com/santiago/moneyctrl/services/UsuarioService.java
@@ -1,12 +1,17 @@
 package com.santiago.moneyctrl.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.santiago.moneyctrl.domain.Usuario;
+import com.santiago.moneyctrl.domain.enuns.TipoRoles;
 import com.santiago.moneyctrl.dtos.UsuarioDTO;
 import com.santiago.moneyctrl.repositories.UsuarioRepository;
+import com.santiago.moneyctrl.security.UserSS;
+import com.santiago.moneyctrl.services.exceptions.AuthorizationException;
+import com.santiago.moneyctrl.services.exceptions.ObjectNotFoundException;
 import com.santiago.moneyctrl.services.interfaces.IServiceValidator;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class UsuarioService extends BaseService<Usuario, UsuarioDTO> implements IServiceValidator {
-	
+
 	@Autowired
 	private BCryptPasswordEncoder crypt;
 
@@ -25,11 +30,27 @@ public class UsuarioService extends BaseService<Usuario, UsuarioDTO> implements 
 	}
 
 	@Override
+	public Usuario findById(Long id) throws ObjectNotFoundException {
+		log.info("[FindByIdUser] - Buscando usuario");
+
+		UserSS userSS = authenticated();
+
+		if (userSS == null || !userSS.hasRole(TipoRoles.ADMIN) && !id.equals(userSS.getId())) {
+			log.error("[FindByIdUser] - Usuario não autorizado.");
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Usuario user = super.findById(id);
+		log.info("[FindByIdUser] - Busca finalizada com sucesso.");
+		return user;
+	}
+
+	@Override
 	public Usuario fromDTO(UsuarioDTO dto) {
 		log.info("[Mapping] - 'UsuarioDTO' to 'Usuario'. Id: " + dto.getId());
 		Usuario usuario = new Usuario(dto.getId(), dto.getNome(), dto.getUsername(), crypt.encode(dto.getPassword()));
 		usuario.setEmail(dto.getEmail());
-		
+
 		log.info("[Mapping] - Mapping finalizado com sucesso.");
 		return usuario;
 	}
@@ -51,12 +72,24 @@ public class UsuarioService extends BaseService<Usuario, UsuarioDTO> implements 
 		log.info("[FindByUnique] - Busca finalizada. Retorno: " + retorno);
 		return retorno;
 	}
-	
+
 	public boolean verificarEmailUnico(String email) {
 		log.info("[FindByUnique] - Buscando email no banco de dados. Email: " + email);
 		boolean retorno = ((UsuarioRepository) this.repository).verificarEmailUnico(email);
 
 		log.info("[FindByUnique] - Busca finalizada. Retorno: " + retorno);
 		return retorno;
+	}
+
+	public static UserSS authenticated() {
+		try {
+			log.info("[Authenticated] - Buscando usuario logado.");
+			UserSS userLogado = (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			log.info("[Authenticated] - UserSS: " + userLogado.toString());
+			return userLogado;
+		} catch (Exception ex) {
+			log.error("[Authenticated] - Usuario não encontrado.");
+			return null;
+		}
 	}
 }
